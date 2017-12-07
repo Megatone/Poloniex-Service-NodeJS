370bda6b176b9139f07ac71a6d83d9bc77c60cfe
Megatone <inacio_pk2@hotmail.com>
2017-12-07 03:15:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,23 +7,14 @@ const settings = require('./config/settings');
 const c = require('./utils/console')(settings.console.limit , settings.console.charSpance);
 const app = require('./app/app')(settings, c);
 
-//psList().then(data => {
-//  if (data.filter(p => p.name == 'mongod.exe').length === 0) {
- //   var childProcess = spawn('powershell.exe', [settings.getMongoCommand(settings.mongo)]).childProcess;
-  //  childProcess.stdout.on('data', (data) => {
-  //    if (!settings.mongo.status) {
-  //      settings.mongo.status = true;
-        mongoose.Promise = global.Promise;
-        mongoose.connect('mongodb://' + settings.mongo.server + ':' + settings.mongo.port + '/' + settings.mongo.database, { useMongoClient: true }).then(() => {
-          c.success('Application database connection with Mongo are be success');
-          c.info('MongoDb Server "' + settings.mongo.server + '"');
-          c.info('MongoDb Port "' + settings.mongo.port + '"');
-          c.info('MongoDb Database "' + settings.mongo.database + '"');
-          app.init();
-        }).catch(err => {
-          c.danger(err);
-        });
-    //  }
-   // });
- // }
-//});
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://' + settings.mongo.server + ':' + settings.mongo.port + '/' + settings.mongo.database, { useMongoClient: true }).then(() => {
+ c.success('Application database connection with Mongo are be success');
+ c.info('MongoDb Server "' + settings.mongo.server + '"');
+ c.info('MongoDb Port "' + settings.mongo.port + '"');
+ c.info('MongoDb Database "' + settings.mongo.database + '"');
+ app.init();
+}).catch(err => {
+ c.danger(err);
+});
+ 
