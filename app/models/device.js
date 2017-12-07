fc16bee58220bb634b612192577d76699e64fbb3
Megatone <inacio_pk2@hotmail.com>
2017-12-07 11:09:29 +0100

    commi

--- app/models/device.js
+++ app/models/device.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 var DeviceSchema = Schema({
     uuid: String,
     token: String,
-    refresh: Number
+    refresh: String
 });
 
 module.exports = mongoose.model('Device', DeviceSchema);   
